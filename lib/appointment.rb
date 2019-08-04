84d537775647f6273001a0db973403e9b05567db
Emily Lillie <emilylillie3@gmail.com>
2019-08-04 17:29:00 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,7 +5,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(patient, doctor, date)
+  def initialize(date, patient, doctor)
     @date = date
     @patient = patient
     @doctor = doctor
